d6ee33081a40399947addb38e04762adf17f0655
Paul-rgbpro <72398502@certus.edu.pe>
2026-09-20 17:51:25 -0500

    feat: persist tasks to json file

--- tareas.js
+++ tareas.js
@@ -1,8 +1,24 @@
+const fs = require("fs");
+const RUTA_ARCHIVO = "./tareas.json";
+
 let tareas = [];
+
+
+if (fs.existsSync(RUTA_ARCHIVO)) {
+    const contenido = fs.readFileSync(RUTA_ARCHIVO, "utf-8");
+    tareas = JSON.parse(contenido);
+}
+
+
+function guardarEnArchivo() {
+    const contenido = JSON.stringify(tareas, null, 2);
+    fs.writeFileSync(RUTA_ARCHIVO, contenido, "utf-8");
+}
 
 function agregarTarea(texto) {
     const nuevaTarea = { texto, completada: false };
     tareas.push(nuevaTarea);
+    guardarEnArchivo();
     return nuevaTarea;
 }
 
@@ -16,6 +32,7 @@ function completarTarea(indice) {
         return undefined;
     }
     tarea.completada = true;
+    guardarEnArchivo();
     return tarea;
 }
 
@@ -24,8 +41,8 @@ function eliminarTarea(indice) {
         return false;
     }
     tareas.splice(indice, 1);
+    guardarEnArchivo();
     return true;
 }
 
-
-module.exports = { agregarTarea, listarTareas, completarTarea, eliminarTarea };
+module.exports = { agregarTarea, listarTareas, completarTarea, eliminarTarea, guardarEnArchivo };
