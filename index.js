53736d5c2f487a62bc898c906c508c11be33684b
Paul-rgbpro <72398502@certus.edu.pe>
2026-09-21 17:48:18 -0500

    feat: add express server with basic endpoints

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("NimbusTrack backend is running!");
+const express = require("express");
+const { agregarTarea, listarTareas} = require("./tareas");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.json({ mensaje: "NimbusTrack API funcionando correctamente" });
+});
+
+app.get("/tareas", (req, res) => {
+    res.json(listarTareas());
+});
+
+app.post("/tareas", (req, res) => {
+    const texto = req.body.texto;
+    const nuevaTarea = agregarTarea(texto);
+    res.json(nuevaTarea);
+});
+
+app.listen(PORT, () => {
+    console.log(`NimbusTrack backend running on port ${PORT}`);
+})
